912b12e2dd5aac40ead38fa417a4660a13bda54d
darkstack <1835601+darkstack@users.noreply.github.com>
2025-01-20 10:25:05 +0100

    Config

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -6,8 +6,8 @@
 local M = {}
 
 M.base46 = {
-	theme = "onedark",
-
+	theme = "gruvchad",
+ --  transparency = true,
 	-- hl_override = {
 	-- 	Comment = { italic = true },
 	-- 	["@comment"] = { italic = true },
@@ -20,5 +20,35 @@ M.base46 = {
 --          lazyload = false
 --      }
 --}
+
+
+M.nvdash = {
+     load_on_startup = true,
+
+     header = {
+"⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⠀⠀⠀⠀",
+"⠀⠀⠀⢸⠛⠒⠒⠀⠤⢤⣤⡔⠚⠉⠀⢸⠀⠀⠀⠀",
+"⠀⠀⠀⢸⡄⢀⠴⣊⠉⠀⢀⡈⠉⠙⠒⠾⣙⡦⠄⠀",
+"⠀⠠⠴⡁⡽⣡⠊⠁⠀⠀⢰⠳⡌⣦⡀⠀⠈⢳⣄⠀",
+"⠀⠀⠀⢸⢱⠃⠀⠀⢀⡴⠃⠀⡿⠃⠙⢦⡀⠘⣏⠁",
+"⠀⠀⠀⡆⢸⡄⢠⣾⠉⠙⡆⠀⠀⠀⣼⡏⠙⣦⣸⠀",
+"⠀⠀⢰⠇⠈⢷⢾⠄⠀⠤⠅⠀⠀⠀⠦⠤⠤⣇⡿⠀",
+"⠀⠀⣼⡆⠀⠀⢸⠀⠀⢀⡄⠒⠒⠒⡆⠀⣠⢞⣡⠀",
+"⠀⠙⣅⣣⣀⣀⣸⣦⣄⣈⣇⣀⣀⣤⠗⢻⣅⡜⠻⠆",
+"⠀⠘⠧⠤⠤⠽⣀⣀⡿⢿⣟⣿⣉⣉⠭⠭⠤⠼⠃⠀",
+"⠀⠀⠀⠀⠀⢄⡀⢠⠇⠀⠉⠘⡇⠁⣀⠄⠠⠄⡀⠀",
+"⠉⠒⠤⣀⠀⠀⡈⣽⠀⠀⠀⠀⢸⡍⣀⠀⠀⡀⠀⠂",
+"⠀⠀⠀⠀⠀⠀⢀⡿⠀⠀⠀⠀⠀⢳⢀⠈⠀⠀⠀⠀",
+"⠀⠀⡠⠂⠀⠀⣰⢧⠌⢄⠖⡔⠡⡞⣧⠀⠈⠑⢄⠀",
+"⠠⠊⠀⠀⠀⠊⠈⠻⡿⠛⠛⠛⠻⠽⠀⠃⠀⠀⠈⠣",
+     },
+
+     buttons = {
+       { txt = "  Find File", keys = "Spc f f", cmd = "Telescope find_files" },
+       { txt = "  Recent Files", keys = "Spc f o", cmd = "Telescope oldfiles" },
+       -- more... check nvconfig.lua file for full list of buttons
+     },
+   }
+
 
 return M
--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,4 +7,10 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+map("n","<C-PageDown>", "<cmd>bnext<cr>", { desc = "Next buffer" })
+map("n","<C-PageUp>", "<cmd>bprevious<cr>",{ desc = "Previous buffer" })
+map('n',"gra", function() vim.lsp.buf.code_action() end, {desc = "vim.lsp.buf.code_action()" })
+map("x","gra",  function() vim.lsp.buf.code_action() end, { desc = "vim.lsp.buf.code_action()" })
+map("n","grn", function() vim.lsp.buf.rename() end ,{desc = "vim.lsp.buf.rename()" })
+map("n","grr", function() vim.lsp.buf.references() end ,{ desc = "vim.lsp.buf.references()" })
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
